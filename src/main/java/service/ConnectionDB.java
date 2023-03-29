278b999327459d4a903ec85c1fb7fecc83540029
Wilde <vitaliwilde@gmail.com>
2023-03-29 15:42:10 +0300

    refresh

--- src/main/java/service/ConnectionDB.java
+++ src/main/java/service/ConnectionDB.java
@@ -6,11 +6,11 @@ import java.sql.DriverManager;
 
 public class ConnectionDB {
     public static Connection getDBConnection(){
-        Connection dbConnectoin = null;
+        Connection dbConnection = null;
 
         try {
             Class.forName("org.postgresql.Driver");
-            dbConnectoin = DriverManager
+            dbConnection = DriverManager
                     .getConnection("jdbc:postgresql://localhost:5432/usermanager",
                             "postgres", "total7749");
         } catch (Exception e) {
@@ -18,7 +18,7 @@ public class ConnectionDB {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
-        return dbConnectoin;
+        return dbConnection;
     }
 }
 
